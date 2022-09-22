984d4f4be9ddcc733fa97b5e78fbdad505baeca8
jid <idczak.jaroslaw@gmail.com>
2022-09-22 11:14:22 +0200

    basic components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/Login'
+import DashLayout from './components/DashLayout'
+import Welcome from './features/Welcome'
+import UsersList from './features/users/UsersList'
+import NotesList from './features/notes/NotesList'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        {/* Will require authentication */}
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>{/* /dash */}
+
+      </Route>
+    </Routes>
   );
 }
 
